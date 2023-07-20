1fc046c30f125fd9b65cbc0493c5a34782ac03e1
Angel <2162556631@qq.com>
2023-07-20 15:10:08 +0800

    Player Run/Walk movements & script

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -10,16 +10,26 @@ public class Character : MonoBehaviour
     private PlayerInput _playerinput;
     private float _verticalVelocity;
     public float Gravity = -9.8f;
+    private Animator _animator;
+
     private void Awake() {
         _cc = GetComponent<CharacterController>();
         _playerinput = GetComponent<PlayerInput>();
+        _animator = GetComponent<Animator>();
     }
 
     private void CalculatePlayerMovement(){
         _movementVelocity.Set(_playerinput.HorizontalInput, 0f, _playerinput.VerticalInput);
         _movementVelocity.Normalize();
         _movementVelocity = Quaternion.Euler(0,-45f,0) * _movementVelocity;
-        _movementVelocity *= MoveSpeed * Time.deltaTime;
+
+        if(Input.GetKey(KeyCode.LeftShift)){
+            _animator.SetFloat("Speed",_movementVelocity.magnitude*5);
+            _movementVelocity *= MoveSpeed * Time.deltaTime * 1.5f;
+        } else{
+            _animator.SetFloat("Speed",_movementVelocity.magnitude);
+            _movementVelocity *= MoveSpeed * Time.deltaTime;
+        }
 
         if(_movementVelocity != Vector3.zero){
             transform.rotation = Quaternion.LookRotation(_movementVelocity);
